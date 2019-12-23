fa3b6e7868ff60861ee78516d65d6f10b83c3d70
Richm <ric5hme45@yahoo.com>
2019-12-23 15:25:33 +0200

    tensor1

--- campus/campus_tuple.py
+++ campus/campus_tuple.py
@@ -3,6 +3,11 @@
 https://courses.campus.gov.il/courses/course-v1:CS+GOV_CS_selfpy101+3_2019/courseware/bb1255da2f294bffb8066472d9db592e/b0713462bbd54813af90aef7c415c0f7/
  Sort  a tuple according to price
 '''
+
+import tensorflow as tf
+hello = tf.constant('Hello, Codacus!')
+sess = tf.Session()
+print(sess.run(hello))
 def getKey(item):
     return item[1]
 
