44c486d7a8807d64f88c8d4d74533ff33a904c8e
Richm <ric5hme45@yahoo.com>
2020-06-13 15:46:12 +0300

    sprite

--- techwithtim/pygame/pygame_basic.py
+++ techwithtim/pygame/pygame_basic.py
@@ -1,41 +1,102 @@
 """
  https://www.pygame.org/ 
- https://techwithtim.net/tutorials/game-development-with-python/pygame-tutorial/pygame-tutorial-movement/
- https://techwithtim.net/tutorials/game-development-with-python/pygame-tutorial/jumping/
+ https://techwithtim.net/tutorials/game-development-with-python/pygame-tutorial/optimization/
 """
 import pygame
 
 pygame.init()
-win = pygame.display.set_mode((500, 500))  # window  500 X 500
+
+win = pygame.display.set_mode((500, 480))
 pygame.display.set_caption("First Game")
 
+walkRight = [pygame.image.load('img/R1.png'), pygame.image.load('img/R2.png'), pygame.image.load('img/R3.png'),
+             pygame.image.load('img/R4.png'), pygame.image.load('img/R5.png'), pygame.image.load('img/R6.png'),
+             pygame.image.load('img/R7.png'), pygame.image.load('img/R8.png'), pygame.image.load('img/R9.png')]
+walkLeft = [pygame.image.load('img/L1.png'), pygame.image.load('img/L2.png'), pygame.image.load('img/L3.png'),
+            pygame.image.load('img/L4.png'), pygame.image.load('img/L5.png'), pygame.image.load('img/L6.png'),
+            pygame.image.load('img/L7.png'), pygame.image.load('img/L8.png'), pygame.image.load('img/L9.png')]
+bg = pygame.image.load('img/bg.jpg')
+char = pygame.image.load('img/standing.png')
+
 x = 50
-y = 50
+y = 400
 width = 40
 height = 60
 vel = 5
 
+clock = pygame.time.Clock()
+
+isJump = False
+jumpCount = 10
+
+left = False
+right = False
+walkCount = 0
+
+
+def redrawGameWindow():
+    global walkCount
+    # We have 9 images for our walking animation, I want to show the same image for 3 frames
+    # so I use the number 27 as an upper bound for walkCount because 27 / 3 = 9. 9 images shown
+    # 3 times each animation.
+    win.blit(bg, (0, 0))
+    if walkCount + 1 >= 27:#one sprite for 3 frames we have 9 sprite  9*3=27
+        walkCount = 0
+
+    if left:
+        # //  quotient without remainder
+        win.blit(walkLeft[walkCount // 3], (x, y))
+        walkCount += 1
+    elif right:
+        win.blit(walkRight[walkCount // 3], (x, y))
+        walkCount += 1
+    else:
+        win.blit(char, (x, y))
+        walkCount = 0
+
+    pygame.display.update()
+
+
 run = True
 
 while run:
-    pygame.time.delay(10)  # delay amount of 0.1 milliseconds.
-    for event in pygame.event.get():  # loop through a list keyboard or mouse events.
-        if event.type == pygame.QUIT:  # The red button in the corner of the window is clicked
-            run = False  # Ends the game loop
-    keys = pygame.key.get_pressed()  # This will give us a dictonary .
-                                     # 1 - key pressed       0 - key not pressed.
+    clock.tick(27)
 
-    if keys[pygame.K_LEFT]  and x > vel:  # Don't move over screen left side .
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT] and x < 500 - vel - width:  # Don't move over screen left side
-        x += vel
-    if keys[pygame.K_UP]    and y > vel:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < 500 - height - vel:
-        y += vel
+        left = True
+        right = False
 
-    win.fill((0, 0, 0))  # Fills the screen with black
-                  # window/surface, color, rect
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-    pygame.display.update()  # This updates the screen so we can see our rectangle
-pygame.quit()  # close our game
+    elif keys[pygame.K_RIGHT] and x < 500 - vel - width:
+        x += vel
+        left = False
+        right = True
+
+    else:
+        left = False
+        right = False
+        walkCount = 0
+
+    if not (isJump):
+        if keys[pygame.K_SPACE]:
+            isJump = True
+            left = False
+            right = False
+            walkCount = 0
+    else:
+        if jumpCount >= -10:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            jumpCount -= 1
+        else:
+            jumpCount = 10
+            isJump = False
+
+    redrawGameWindow()
+
+pygame.quit()
