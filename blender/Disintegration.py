018552b4f84c613c0d3feb0b7e5636e9c52af684
Richm <ric5hme45@yahoo.com>
2020-12-24 16:30:05 +0200

    explosain

--- blender/Disintegration.py
+++ blender/Disintegration.py
@@ -9,7 +9,9 @@ bpy.context.scene.render.engine = 'CYCLES'#Change  to  cycle
 
 loca = (0, 0, 0)
 scal=(1, 1, 1)
-bpy.ops.mesh.primitive_cube_add(align='WORLD', location= loca, scale=scal)
+bpy.ops.mesh.primitive_monkey_add(enter_editmode=False, align='WORLD', location= loca, scale=scal)
+
+#bpy.ops.mesh.primitive_cube_add(align='WORLD', location= loca, scale=scal)
 bpy.ops.object.editmode_toggle() #  toggle edit mode
 
 bpy.ops.mesh.edge_split(type='EDGE')#Split by edges alt M
@@ -24,29 +26,33 @@ new_mat.use_nodes= True# Show the marials  node
 nodes = new_mat.node_tree.nodes# shortcut  variable
 
 bsdf = nodes.get("Principled BSDF")  
-bsdf.location = (200,-10)
+bsdf.location = (400,-10)
 
 
 material_output = nodes.get("Material Output")
-material_output.location = (600,-30)
+material_output.location = (700,-30)
 
 #Add  vector displacment  node
 node_vec_disp = nodes.new(type = 'ShaderNodeVectorDisplacement')
 node_vec_disp.inputs[0].default_value =(0.78,0.3,1.0,55)#color
 node_vec_disp.inputs[1].default_value = 1#strength
-node_vec_disp.location = (-190,20)#node   location 
+node_vec_disp.location = (130,60)#node   location 
 
 #Add  texture cordinate node 
 node_TexCoord = nodes.new(type = 'ShaderNodeTexCoord') 
-node_TexCoord .location = (-600,-50) 
+node_TexCoord .location = (-200,-50) 
 
  #Add  texture value node 
 node_Val = nodes.new(type="ShaderNodeValue") 
-node_Val.location = (-360,-160) 
+node_Val.location = (-80,-410) 
+
+#add math node  subtruct
+node_math = nodes.new(type="ShaderNodeMath") 
+node_math.inputs[0].default_value =1
+node_math.operation = 'SUBTRACT'
 
 
-
-
+node_math.location = (110,-270)
 
 #Links
 links = new_mat.node_tree.links
@@ -60,8 +66,8 @@ new_link = links.new(node_TexCoord.outputs[0], node_vec_disp.inputs[0])
 
 new_link = links.new(node_Val.outputs[0], node_vec_disp.inputs[2])      
 #4:22
-
-new_link = links.new(node_Val.outputs[0],  bsdf.inputs[19]) 
+new_link = links.new(node_math.outputs[0],  bsdf.inputs[19]) 
+new_link = links.new(node_Val.outputs[0],  node_math.inputs[1]) 
 bpy.context.object.active_material.cycles.displacement_method = 'BOTH'
 #change merial pro/setting/Displacement  to  displacment and bump 
 
