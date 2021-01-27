3467400e4d7747220659a9cc83b8b7bc959131fe
danizalm05 <danizalm05@gmail.com>
2021-01-27 18:49:29 +0200

    photo-3d

--- blender/David-Mignot.py
+++ blender/David-Mignot.py
@@ -2,9 +2,52 @@
 # Artistic Coding in Blender by David Mignot
 import   bpy
 import random
+
+mat1 = bpy.data.materials.new(name="Material001") #set new material to variable
+mat2 = bpy.data.materials.new(name="Material002")  
+
 spacing  = 2.2
-for x in  range(10):
-    for y in  range(10):
+for x in  range(8):
+    for y in  range(4):
+       s = (1, 1, 5*random.random())
        location=(x * spacing, y * spacing,random.random()*2) 
-       bpy.ops.mesh.primitive_cube_add(location=location)
+       bpy.ops.mesh.primitive_cube_add(location=location,scale = s)
+       
 #3:45  shader
+
+#activeObject = bpy.context.active_object #Set active object to variable
+       item = bpy.context.active_object #Set active object to variable
+    
+       if random.random()<0.1:
+            item.data.materials.append(mat1) #add the material to the object
+            bpy.context.object.active_material.diffuse_color = (25 , 233 , 2,0) #change color
+          # bpy.context.object.active_material.intensity = (11)
+       else: 
+             item.data.materials.append(mat2) #add the material to the object
+             bpy.context.object.active_material.diffuse_color = (0.7,0.4,233,0) #change color   
+             #bpy.context.object.active_material.specular_color = (235, 45, 0.5) 
+             
+#bpy.context.object.active_material.diffuse_color=(0.7,0.4,0.8,0)             
+"""
+ bpy.context.object.active_material.diffuse_
+                                               color
+                                               fresnel
+                                               fresnel_factor
+                                               intensity
+                                               ramp
+                                               ramp_blend
+                                               ramp_factor
+                                               ramp_input
+                                               shader
+                                               toon_size
+                                               toon_smooth
+>>> bpy.context.object.active_material.diffuse_color=(0.7,0.4,0.8)
+
+
+
+"""   
+
+
+
+ 
+          
--- blender/stairs.py
+++ blender/stairs.py
@@ -15,3 +15,21 @@ for i in range(0,10):
 
 
  
+from random import randint 
+# Generate 50 cubes in random locations
+for i in range(50): 
+      l = [ randint( -30, 0 )  for axis in 'xyz' ]
+      bpy.ops.mesh.primitive_cube_add(     location = l )
+      
+      
+      
+from math import sin 
+# Generate 50 cubes along a sin curve 
+for i in range(50):
+    x, y, z = 10, i+22, sin( i )+15 
+    bpy.ops.mesh.primitive_cube_add( location = (x, y, z) )
+    
+    
+    
+  
+           
